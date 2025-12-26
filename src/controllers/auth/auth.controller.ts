321ae3b0c6753b46f74bcdd60d8c9ff9e99983ff
Ferdinand05 <fkoryanto@gmail.com>
2025-12-26 21:04:40 +0700

    create : authentication

---
+++ src/controllers/auth/auth.controller.ts
@@ -0,0 +1,75 @@
+import { Request, Response } from "express";
+import z from "zod";
+import User from "../../models/User";
+import jwt from "jsonwebtoken";
+import { IUser } from "../../types/user";
+import bcrypt from "bcrypt";
+
+//
+export async function login(req: Request, res: Response) {
+  const loginSchema = z.object({
+    email: z.email(),
+    password: z.string().min(7),
+  });
+
+  const parsed = loginSchema.safeParse(req.body);
+
+  if (!parsed.success) return res.status(400).json({ error: parsed.error });
+
+  const { email, password } = parsed.data;
+
+  const user: IUser | null = await User.findOne({ email }).select("+password");
+
+  if (!user) return res.status(404).json({ error: "Email / Password is not valid" });
+
+  const jwt_secret = process.env.JWT_SECRET;
+
+  if (!jwt_secret) throw Error("JWT SECRET TOKEN, is not found.");
+
+  if (!user.password) return res.status(404).json({ error: "Invalid credentials" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) return res.status(500).json({ error: "Invalid credentials" });
+
+  const token = jwt.sign(
+    {
+      user_id: user._id,
+      username: user.username,
+      role: user.role,
+      email: user.email,
+    },
+    jwt_secret,
+    {
+      expiresIn: "1d",
+    }
+  );
+
+  return res.status(200).json({
+    success: "Login Success!",
+    token,
+  });
+}
+
+export async function register(req: Request, res: Response) {
+  const registerSchema = z.object({
+    username: z.string().min(3),
+    email: z.email(),
+    password: z.string().min(7).trim(),
+    telepon: z.string().max(15).min(3),
+  });
+
+  const parsed = registerSchema.safeParse(req.body);
+
+  if (!parsed.success) return res.status(400).json({ error: parsed.error });
+  const { username, email, password, telepon } = parsed.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    await User.create({ username, email, password: hashedPassword, telepon });
+    return res.status(201).json({ success: "Account has been created successfully." });
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import categoryRoutes from "./category.routes";
 import medicineRoutes from "./medicine.routes";
+import authRoutes from "./auth.routes";
 import userRoutes from "./user.routes";
 const router = Router();
 
 router.use("/categories", categoryRoutes);
 router.use("/medicines", medicineRoutes);
 router.use("/users", userRoutes);
-
+router.use("/", authRoutes);
 export default router;
