037fa83daaa622bdd747133c851fda9d460bbbca
Ferdinand05 <fkoryanto@gmail.com>
2025-12-27 09:35:51 +0700

    fix :  postion for dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import routes from "./routes/index";
 import morgan from "morgan";
-import dotenv from "dotenv";
-dotenv.config();
 
 const app = express();
 app.use(morgan("dev"));
